2a7ed5dac22adee935bff6f98753f3f7f7407f68
takefumi-yoshii <takefumi.yoshii@dena.jp>
2019-07-31 01:09:11 +0900

    update design

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,11 +1,7 @@
 import React from "react"
-import styled from "styled-components"
-import { Link } from "gatsby"
 import { ArticlePublishedDate } from "./ArticlePublishedDate"
-import { HeadGroup, Title, Body, FootGroup } from "./ArticleStyle"
-import { ArticleAvatarImage } from "./ArticleAvatarImage"
-import { rhythm } from "../utils/typography"
-import AuthorListItem from "./AuthorListItem"
+import { HeadGroup, Title, Body } from "./ArticleStyle"
+import { ArticleFootGroup } from "./ArticleFootGroup"
 
 type Author = {
   id: string
@@ -22,47 +18,26 @@ type Props = {
   author: Author
 }
 
-const AvatarImage = styled.img`
-  width: 150px;
-  height: 150px;
-  margin: ${rhythm(1 / 2)};
-  min-width: 50;
-`
+const View: React.FC<Props> = props => (
+  <>
+    <HeadGroup>
+      <ArticlePublishedDate publishedAt={props.publishedAt} />
+      <div>
+        <Title>{props.title}</Title>
+      </div>
+    </HeadGroup>
+    <Body
+      dangerouslySetInnerHTML={{
+        __html: props.body,
+      }}
+    />
+    <ArticleFootGroup
+      title={props.title}
+      publishedAt={props.publishedAt}
+      body={props.body}
+      author={props.author}
+    />
+  </>
+)
 
-const Container = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`
-
-export const Article: React.FC<Props> = props => {
-  const { author } = props
-  const { id, twitter, bio, github, name } = author
-  const imageUrl = `https://github.com/${id}.png`
-  return (
-    <>
-      <HeadGroup>
-        <ArticlePublishedDate publishedAt={props.publishedAt} />
-        <div>
-          <Title>{props.title}</Title>
-        </div>
-      </HeadGroup>
-      <Body
-        dangerouslySetInnerHTML={{
-          __html: props.body,
-        }}
-      />
-      <hr />
-      <Link to={`/author/${props.author.id}`}>
-        <FootGroup>
-          <ArticleAvatarImage imageUrl={imageUrl} />
-          <Container>
-            <h2>{author.name}</h2>
-            <p>{author.bio}</p>
-          </Container>
-        </FootGroup>
-      </Link>
-    </>
-  )
-}
+export { View as Article }
